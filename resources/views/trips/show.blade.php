c78c0072e8fac35ca5bc43b067af673c8389af58
Joucke Hempenius <joucke@gmail.com>
2022-10-31 16:18:22 +0100

    Update region update / display

--- resources/views/trips/show.blade.php
+++ resources/views/trips/show.blade.php
@@ -10,7 +10,7 @@
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 flex justify-between bg-white border-b border-gray-200">
           <div id="user-list">
-            <!-- TODO: make this more interactive, alpine it -->
+            <!-- TODO: make this more interactive, alpine it / move to header? -->
             @if ($trip->users->count() > 1)
             {{ __('Trip participants') }}:
             @foreach ($trip->users as $user)
@@ -58,62 +58,89 @@
         trip: {{ $trip }},
         showForm: false,
         newRegion: '',
+        display: (() => location.hash ? location.hash.split(',') : ['#overview'])(),
       }" class="mt-6 flex flex-1 bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 flex flex-wrap w-full bg-white border-b border-gray-200">
-          <div id="regions" class="w-full lg:w-1/5 xl:w-1/6 pr-6">
-            <template x-for="region in trip.regions">
-              <div cy-id="region-row" class="py-2 gap-3 group">
-                <!-- TODO: delete icon inside div around p.w-full -->
-                <p class="w-full"
-                  @blur="e => $store.region.update(
-                    '{{ route('trips.regions.store', $trip) }}/'+region.id,
-                    '{{ csrf_token() }}',
-                    { title: e.target.innerText}
-                  ).then(data => trip.regions = data)"
-                  contenteditable="true"
-                  @input="(e) => region.title = e.target.innerText"
-                  x-bind:id="`region-title-${region.id}`"
-                  x-text="region.title"></p>
-                <input class="w-full text-xs rounded-lg" x-bind:id="'arrival-at-' + region.id" type="datetime-local" step="1" name="arrival_at" x-bind:value="region.arrival_at" x-on:change="e => $store.region.update(
-                    '{{ route('trips.regions.store', $trip) }}/'+region.id,
-                    '{{ csrf_token() }}',
-                    {arrival_at: e.target.value}
-                  ).then(data => trip.regions = data)">
-                <form x-bind:action="'{{ route('trips.regions.store', $trip) }}/'+region.id" method="post">
-                  @csrf
-                  @method('DELETE')
-                  <button class="hidden group-hover:inline-flex items-center justify-center rounded-full bg-white text-red-500 border-2 border-red-500 w-6 h-6 p-2 font-bold">&times;</button>
-                </form>
-                <button class="items-center border-2 px-4 py-1 rounded-lg">{{ __("add place") }}</button>
-              </div>
-            </template>
-            <template x-if="! showForm">
-              <x-primary-button x-on:click="showForm = true" id="add-region">add region</x-primary-button>
-            </template>
-            <template x-if="showForm">
-              <!-- TODO: adjust width -->
-              <div class="w-full">
-                <div>
-                  <x-input-label for="region-title" :value="__('Region')" />
-                  <x-text-input autofocus x-init="$watch('newRegion', title => $store.search.region(title))" class="mt-1 w-full" type="text" id="region-title" x-model="newRegion">
-                  </x-text-input>
+          <template x-if="display.includes('#overview')">
+            <div id="regions" class="w-full lg:w-1/5 xl:w-1/6 pr-6">
+              <template x-for="region in trip.regions">
+                <div cy-id="region-row" class="py-2 gap-3 group">
+                  <a x-bind:id="'link-region-' + region.id" x-bind:href="'#region,'+region.id" x-text="region.title" @click="display = ['#region', region.id]"></a>
                 </div>
-                <div id="region-result" class="flex flex-wrap gap-3 mt-3">
-                  <template x-for="region in $store.search.results.regions">
-                    <button class="rounded-full bg-green-300 text-xs px-2 py-1" x-bind:title="region.display_name" x-text="region.display_name.split(', ')[0]" @click="$store.region.create(
-                        '{{ route('trips.regions.store', $trip) }}',
-                        '{{ csrf_token() }}',
-                        region
-                      ).then(data => {
-                        trip.regions = data
-                        newRegion = ''
-                        showForm = !showForm
-                      })"></button>
-                  </template>
+              </template>
+              <template x-if="! showForm">
+                <x-primary-button x-on:click="showForm = true" id="add-region">add region</x-primary-button>
+              </template>
+              <template x-if="showForm">
+                <div class="w-full">
+                  <div>
+                    <x-input-label for="region-title" :value="__('Region')" />
+                    <x-text-input autofocus x-init="$watch('newRegion', title => $store.search.region(title))" class="mt-1 w-full" type="text" id="region-title" x-model="newRegion">
+                    </x-text-input>
+                  </div>
+                  <div id="region-result" class="flex flex-wrap gap-3 mt-3">
+                    <template x-for="region in $store.search.results.regions">
+                      <button class="rounded-full bg-green-300 text-xs px-2 py-1" x-bind:title="region.display_name" x-text="region.display_name.split(', ')[0]" @click="$store.region.create(
+                          '{{ route('trips.regions.store', $trip) }}',
+                          '{{ csrf_token() }}',
+                          region
+                        ).then(data => {
+                          trip.regions = data
+                          newRegion = ''
+                          showForm = !showForm
+                        })"></button>
+                    </template>
+                  </div>
                 </div>
-              </div>
-            </template>
-          </div>
+              </template>
+            </div>
+          </template>
+          <template x-if="display.includes('#region')">
+            <div class="w-full lg:w-1/5 xl:w-1/6 pr-6" x-data="{
+              region: (() => trip.regions.find(r => r.id == display[1]))(),
+            }">
+              <h1 x-bind:id="'header-region-' + region.id" class="-mx-4 py-2 text-gray-500 flex items-center justify-between gap-2">
+                <div class="gap-2 truncate">
+                  <a id="back-to-trip-overview" href="#overview" @click="display = ['#overview']" class="w-6 h-6 inline-flex items-center flex-shrink-0 justify-center rounded-md border border-gray-200 text-gray-200 hover:text-gray-500">&lt;</a>
+                  <span class="text-xs uppercase tracking-widest truncate" x-text="region.title"></span>
+                </div>
+                <template x-if="!display.includes('edit')">
+                  <a x-bind:id="'edit-region-' + region.id" class="w-6 h-6 inline-flex items-center justify-center flex-shrink-0 rounded-md border border-gray-200 text-gray-200 hover:text-gray-500" x-bind:href="`#region,${region.id},edit`" @click="display = ['#region', region.id, 'edit']">e</a>
+                </template>
+                <template x-if="display.includes('edit')">
+                  <a class="w-6 h-6 inline-flex items-center justify-center flex-shrink-0 rounded-md border border-gray-200 text-gray-200 hover:text-gray-500" x-bind:href="`#region,${region.id}`" @click="display = ['#region', region.id]">&times;</a>
+                </template>
+              </h1>
+              <template x-if="display.includes('edit')">
+                <div x-bind:id="'edit-region-' + region.id" class="group -mx-4">
+                  <x-input-label for="title" :value="__('Title')" />
+                  <x-text-input x-model="region.title" class="block px-4 py-2 w-full" type="text" name="title" x-bind:id="`region-title-${region.id}`" @change="e => $store.region.update(
+                      '{{ route('trips.regions.store', $trip) }}/'+region.id,
+                      '{{ csrf_token() }}',
+                      { title: e.target.value}
+                    ).then(data => trip.regions = data)" required autofocus />
+                  <x-input-label class="mt-2" for="arrival_at" :value="__('Arrival at')" />
+                  <x-text-input x-model="region.arrival_at" class="block px-4 py-2 w-full" type="datetime-local" step="1" name="arrival_at" x-bind:id="`region-arrival_at-${region.id}`" @change="e => $store.region.update(
+                      '{{ route('trips.regions.store', $trip) }}/'+region.id,
+                      '{{ csrf_token() }}',
+                      { arrival_at: e.target.value}
+                    ).then(data => trip.regions = data)" />
+                  <form x-bind:action="'{{ route('trips.regions.store', $trip) }}/'+region.id" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button class="hidden group-hover:inline-flex items-center justify-center rounded-full bg-white text-red-500 border-2 border-red-500 w-6 h-6 p-2 font-bold">&times;</button>
+                  </form>
+                </div>
+              </template>
+              <template x-if="!display.includes('edit')">
+                <div class="-mx-4 pl-2">
+                  <p>list places in region</p>
+                  <button class="items-center border-2 px-4 py-1 rounded-lg">{{ __("add place") }}</button>
+                </div>
+                <!-- TODO: add place to region -->
+              </template>
+            </div>
+          </template>
           <div class="w-full lg:w-4/5 xl:w-5/6">
             <div id="map" class="w-full h-full" x-init="$watch('trip', (trip => $store.map.update(trip))); $nextTick(() => $store.map.show(trip));"></div>
           </div>
--- tests/cypress/integration/regions.cy.js
+++ tests/cypress/integration/regions.cy.js
@@ -1,8 +1,8 @@
 describe('regions - a logged in user', () => {
   beforeEach(() => {
-    cy.refreshDatabase();
+    cy.refreshDatabase()
     cy.login()
-  });
+  })
 
   it('can add a region to a trip', () => {
     cy.seed('TripSeeder')
@@ -26,6 +26,93 @@ describe('regions - a logged in user', () => {
       .should('have.length', 1)
   })
 
+  it('can switch from overview to region view', () => {
+    cy.seed('TripSeeder')
+    cy.php(`
+      App\\Models\\Region::factory()->create([
+        'title' => 'Region',
+        'trip_id' => 1,
+      ]);
+    `)
+    cy.visit(Cypress.Laravel.route('trips.show', {trip: 1}))
+      .get('a#link-region-1')
+      .click()
+
+    cy.get('h1#header-region-1')
+      .contains('Region')
+  })
+
+  it('can switch from region view to overview', () => {
+    cy.seed('TripSeeder')
+    cy.php(`
+      App\\Models\\Region::factory()->create([
+        'title' => 'Region',
+        'trip_id' => 1,
+      ]);
+    `)
+    cy.visit(Cypress.Laravel.route('trips.show', {trip: 1}) + '#region,1')
+
+    cy.get('h1#header-region-1')
+      .contains('Region')
+
+    cy.get('a#back-to-trip-overview')
+      .click()
+
+    cy.get('#regions [cy-id="region-row"]')
+      .contains('Region')
+  })
+
+  it('can open the edit form for fields of a region', () => {
+    cy.seed('TripSeeder')
+    cy.php(`
+      App\\Models\\Region::factory()->create([
+        'title' => 'Original title',
+        'trip_id' => 1,
+      ]);
+    `)
+
+    cy.visit(Cypress.Laravel.route('trips.show', { trip: 1 }) + '#region,1')
+
+    cy.get('a#edit-region-1')
+      .click()
+
+    cy.location()
+      .should(loc => {
+        expect(loc.hash).to.eq('#region,1,edit')
+      })
+
+    cy.get('#edit-region-1 input:visible')
+      .its('length')
+      .should('eq', 2)
+  })
+
+  it('can close the edit form for fields of a region', () => {
+    cy.seed('TripSeeder')
+    cy.php(`
+      App\\Models\\Region::factory()->create([
+        'title' => 'Original title',
+        'trip_id' => 1,
+      ]);
+    `)
+
+    cy.visit(Cypress.Laravel.route('trips.show', { trip: 1 }) + '#region,1,edit')
+
+    cy.get('h1#header-region-1>a')
+      .contains('×')
+      .click()
+
+    cy.location()
+      .should(loc => {
+        expect(loc.hash).to.eq('#region,1')
+      })
+
+    cy.get('input#region-title-1')
+      .should('not.exist')
+
+    cy.get('input#region-arrival_at-1')
+      .should('not.exist')
+  })
+
   it('can update the title of a region', () => {
     cy.seed('TripSeeder')
     cy.php(`
@@ -38,21 +125,22 @@ describe('regions - a logged in user', () => {
     cy.intercept('patch', Cypress.Laravel.route('trips.regions.update', {trip: 1, region: 1}))
       .as('region-patch')
 
-    cy.visit(Cypress.Laravel.route('trips.show', {trip: 1}))
+    cy.visit(Cypress.Laravel.route('trips.show', {trip: 1}) + '#region,1')
 
-    cy.get('p#region-title-1')
-      .contains(/original title/i)
+    cy.get('a#edit-region-1')
       .click()
+
+    cy.get('input#region-title-1')
       .clear()
       .type('A new title')
-
       .blur()
 
     cy.wait('@region-patch')
 
-    cy.reload()
-    cy.get('p#region-title-1')
+    cy.get('h1#header-region-1')
       .contains('A new title')
+
+    // TODO: confirm the new title still exists after a reload
   })
 
   it('can updated the arrival datetime of a region', () => {
@@ -76,12 +164,27 @@ describe('regions - a logged in user', () => {
 
     cy.visit(Cypress.Laravel.route('trips.show', { trip: 1 }))
 
-    cy.get('input#arrival-at-2')
+    cy.get('[cy-id="region-row"]')
+      .first()
+      .contains('First region')
+
+    cy.get('[cy-id="region-row"]')
+      .eq(1)
+      .contains('Second region')
+      .click()
+
+    cy.get('a#edit-region-2')
+      .click()
+
+    cy.get('input#region-arrival_at-2')
       .clear()
       .type('2022-07-05T10:00:00')
       .blur()
 
     cy.wait('@region-patch')
+
+    cy.get('a#back-to-trip-overview')
+      .click()
 
     cy.get('[cy-id="region-row"]')
       .first()
